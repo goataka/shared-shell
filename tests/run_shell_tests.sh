f3087541add42dbc5130456f29a14ad8f9745322
GOAMI_Takaaki <tkgoami@gmail.com>
2025-07-04 11:54:09 +0900

    Add log start/end

--- tests/run_shell_tests.sh
+++ tests/run_shell_tests.sh
@@ -10,27 +10,37 @@ find_test_scripts() {
 }
 
 run_test_scripts() {
-  local -r search_dir="${1}"
+  local -r test_scripts="${1}"
   local -i all_passed=0
 
+  log_info "Test scripts started."
+
   while IFS= read -r test_script; do
-    if ! bash "${test_script}"; then
-      log_debug "${test_script}: [FAIL]"
+    if [ -z "$test_script" ]; then
+      continue
+    fi
+    if ! bash "$test_script"; then
+      log_debug "$test_script: [FAIL]"
       all_passed=1
     fi
-  done < <(find_test_scripts "${search_dir}")
+  done <<< "${test_scripts}"
 
+  if ! (( all_passed == 0 )); then
+    log_error "Tests scripts: [FAIL]"
+  fi
+
+  log_info "Test scripts completed."
   return $all_passed
 }
 
 main() {
   local search_dir="${1:-$(pwd)}"
 
-  if ! run_test_scripts "${search_dir}"; then
-    local -r abs_search_dir="$(cd "${search_dir}" && pwd)"
-    log_error "Tests in ${abs_search_dir}: [FAIL]"
-    exit 1
-  fi
+  local -r abs_search_dir="$(cd "${search_dir}" && pwd)"
+  log_info "Search directory: ${abs_search_dir}"
+
+  local -r test_scripts=$(find_test_scripts "${search_dir}")
+  run_test_scripts "${test_scripts}"
 }
 
 main "$@"
